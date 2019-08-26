5b4a6a857229aeda52ec9b333d42d33990871f0e
ethanvu <ethanhuynhvu@gmail.com>
2019-08-25 20:11:58 -0400

    Add BASE_TWITCHMETRICS_URL and add some minor edits to other URLs

--- clip9/constants.py
+++ clip9/constants.py
@@ -1,4 +1,5 @@
 """Contains constants for clip9"""
 
-BASE_OAUTH2_URL = 'https://id.twitch.tv/oauth2'
-BASE_KRAKEN_URL = 'https://api.twitch.tv/kraken'
+BASE_OAUTH2_URL = 'https://id.twitch.tv/oauth2/'
+BASE_KRAKEN_URL = 'https://api.twitch.tv/kraken/'
+BASE_TWITCHMETRICS_URL = 'https://www.twitchmetrics.net/'
