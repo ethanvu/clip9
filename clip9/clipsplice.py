3a21d22dd5888ea76d01ec3c523182450c8a7385
ethanvu <ethanhuynhvu@gmail.com>
2019-09-06 21:56:53 -0400

    Download clips

--- clip9/clipsplice.py
+++ clip9/clipsplice.py
@@ -3,14 +3,49 @@
 import logging
 import re
 
+import requests
 
 class ClipSplicer():
     """Downloads and splices Twitch clips into a video."""
 
     def __init__(self, clips_list):
         self.clips_list = clips_list
-    
+
+
     def _get_clip_src_url(self, thumbnail_url):
         """Gets the source url of a clip given its thumbnail url."""
+        logging.info(f"Getting clip source URL from {thumbnail_url}")
+        if (re.match('https://clips-media-assets.twitch.tv/'
+                     '[0-9][0-9][0-9][0-9]+-preview-480x272.jpg',
+                     thumbnail_url) is None):
+            raise ValueError(f"{thumbnail_url} is an invalid clip thumbnail "
+                             f"URL to parse")
         nums = re.findall('[0-9][0-9][0-9][0-9]+', thumbnail_url)
-        return f'https://clips-media-assets2.twitch.tv/AT-{nums[0]}-640x360.mp4'
+        logging.info(f"Found nums {nums[0]}")
+        return (f'https://clips-media-assets2.twitch.tv/AT-{nums[0]}'
+                 '-640x360.mp4')
+
+
+    def _download_clip(self, clip, path):
+        """Downloads a clip as an MP4 file.
+
+        :param clip: Info of a clip recieved from the Twitch Helix API.
+        :param path: Path to save the clip in.
+        """
+        logging.info(f"Downloading clip {clip['id']}")
+        clip_src_url = self._get_clip_src_url(clip['thumbnail_url'])
+        logging.info(f"Clip source URL: {clip_src_url}")
+        resp = requests.get(clip_src_url, stream=True)
+        if (resp.status_code >= 400):
+            logging.error(f"Error when downloading clip {resp.status_code}")
+            resp.raise_for_status()
+        with open(f'{path}/{clip["id"]}.mp4', 'wb') as f:
+            for chunk in resp.iter_content(chunk_size=1024*1024):
+                if chunk:
+                    f.write(chunk)
+        logging.info(f"Downloaded {clip['id']}.mp4")
+
+
+    def splice(self, path='./'):
+        """Splices the clips in clips_list into one video."""
+        pass
--- tests/test_clipsplice.py
+++ tests/test_clipsplice.py
@@ -1,14 +1,19 @@
-"""Tests the clipsplice module"""
+"""Tests the clipsplice module."""
 
 from clip9.clipsplice import ClipSplicer
 
+from pathlib import Path
+
 import pytest
+import requests
+import responses
 
 example_clip_list = [
     {
         'id': 'AwkwardHelplessSalamanderSwiftRage',
         'url': 'https://clips.twitch.tv/AwkwardHelplessSalamanderSwiftRage',
-        'embed_url': 'https://clips.twitch.tv/embed?clip=AwkwardHelplessSalamanderSwiftRage',
+        'embed_url': 'https://clips.twitch.tv/'
+                     'embed?clip=AwkwardHelplessSalamanderSwiftRage',
         'broadcaster_id': '67955580',
         'broadcaster_name': 'ChewieMelodies',
         'creator_id': '53834192',
@@ -19,9 +24,19 @@ example_clip_list = [
         'title': 'babymetal',
         'view_count': 10,
         'created_at': '2017-11-30T22:34:18Z',
-        'thumbnail_url': 'https://clips-media-assets.twitch.tv/157589949-preview-480x272.jpg'
+        'thumbnail_url': 'https://clips-media-assets.twitch.tv/'
+                         '157589949-preview-480x272.jpg'
     }
 ]
+
+exmaple_clip_resp_bad_clip_num = """<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+    <Code>AccessDenied</Code>
+    <Message>Access Denied</Message>
+    <RequestId>AF1E1AFC1E201EFF</RequestId>
+    <HostId>LXM6CCdLUm9HptbJqr3+cx8tUe5YR5K6v0BXBS+yiIfc04Pcvb3a83t+oFmcCmd3QR2ehVz3bxw=</HostId>
+</Error>
+"""
 
 
 def test__get_clip_src_url_valid_thumbnail_url_ret_src():
@@ -29,3 +44,67 @@ def test__get_clip_src_url_valid_thumbnail_url_ret_src():
     src_url = splicer._get_clip_src_url(example_clip_list[0]['thumbnail_url'])
     assert ('https://clips-media-assets2.twitch.tv/AT-157589949-640x360.mp4'
             == src_url)
+
+
+def test__get_clip_src_url_invalid_thumbnail_url_hostname_throws_exception():
+    splicer = ClipSplicer(example_clip_list)
+    with pytest.raises(ValueError):
+        src_url = splicer._get_clip_src_url('https://twitch.tv/'
+                                            '157589949-preview-480x272.jpg')
+
+
+def test__get_clip_src_url_invalid_thumbnail_url_path_nums_throws_exception():
+    splicer = ClipSplicer(example_clip_list)
+    with pytest.raises(ValueError):
+        src_url = splicer._get_clip_src_url('https://clips-media-assets.'
+                                            'twitch.tv/15-preview-480x272.jpg')
+
+
+def test__get_clip_src_url_invalid_thumbnail_url_path_else_throws_exception():
+    splicer = ClipSplicer(example_clip_list)
+    with pytest.raises(ValueError):
+        src_url = splicer._get_clip_src_url('https://clips-media-assets.'
+                                            'twitch.tv/157589949-preview.jpg')
+
+
+@responses.activate
+def test__download_clip_valid_url_success():
+    path = './'
+    responses.add(responses.GET,
+                  'https://clips-media-assets2.twitch.tv/'
+                  'AT-157589949-640x360.mp4',
+                  body='a',
+                  status=200,
+                  content_type='binary/octet-stream')
+    splicer = ClipSplicer(example_clip_list)
+    splicer._download_clip(example_clip_list[0], path)
+    clip_file = Path(f'{path}{example_clip_list[0]["id"]}.mp4')
+    assert clip_file.is_file()
+    clip_file.unlink()
+
+
+@responses.activate
+def test__download_clip_invalid_path_throws_exception():
+    path = 'badpath/'
+    responses.add(responses.GET,
+                  'https://clips-media-assets2.twitch.tv/'
+                  'AT-157589949-640x360.mp4',
+                  body='a',
+                  status=200,
+                  content_type='binary/octet-stream')
+    splicer = ClipSplicer(example_clip_list)
+    with pytest.raises(FileNotFoundError):
+        splicer._download_clip(example_clip_list[0], path)
+
+
+@responses.activate
+def test__download_clip_invalid_url_throws_exception():
+    responses.add(responses.GET,
+                  'https://clips-media-assets2.twitch.tv/'
+                  'AT-157589949-640x360.mp4',
+                  body=exmaple_clip_resp_bad_clip_num,
+                  status=403,
+                  content_type='application/xml')
+    splicer = ClipSplicer(example_clip_list)
+    with pytest.raises(requests.HTTPError):
+        splicer._download_clip(example_clip_list[0], './')
